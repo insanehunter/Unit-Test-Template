7ab77c81c8fc554b6258f6aecabe6f4dd0189102
Sergei Cherepanov <s.cherepanov@gmail.com>
2013-01-04 16:05:28 +0400

    Removed imports as they should be in Prefix.pch

--- OSX/OCUnit test case.xctemplate/___FILEBASENAME___.m
+++ OSX/OCUnit test case.xctemplate/___FILEBASENAME___.m
@@ -1,22 +1,19 @@
 /*
  * (c) <#__MyCompanyName__#>, 2013
  */
-#define HC_SHORTHAND
-#define MOCKITO_SHORTHAND
-#import <SenTestingKit/SenTestingKit.h>
-#import <OCHamcrest/OCHamcrest.h>
-#import <OCMockito/OCMockito.h>
-
-// Class under test
 #import "<#Class Header#>"
-
 
 @interface ___FILEBASENAMEASIDENTIFIER___ : SenTestCase
 @end
 
 @implementation ___FILEBASENAMEASIDENTIFIER___
-- (void) testExample
+- (void) test<#Name#>
 {
+	// given
+	
+	// when
+	
+	// then
     STFail(@"Unit tests are not implemented yet in ___FILEBASENAMEASIDENTIFIER___");
 }
 @end
